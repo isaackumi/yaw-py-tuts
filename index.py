74e986ced898195491cbf06f5db22632e1d12745
isaackumi <isaac.kumi@ashesi.edu.gh>
2022-10-10 19:15:22 +0000

    Intro

---
+++ index.py
@@ -0,0 +1,80 @@
+
+# print("Hello, world!")
+# # python as calculator
+
+# ''' This is a docstring '''
+
+# print("Hello, world!")
+# print('Hello, world')
+
+# print(1+1)
+# print(2**2)
+# # interger division
+# print(3/2)
+
+# # Float division
+# print(3//2)
+
+# print(1*2)
+
+# DATA TYPES
+
+# string : (str) example "Hello"
+# Integer : (int) example 5
+# Float : (float) example 2.6
+# Boolean : (bool) example True or False
+
+
+
+# List : (list) example ["Johnson","Larbi","Yaw"]
+# Dictionary : (dict) example {"Name":"John"}
+# Tuples :  example (1,2)
+
+
+
+#  VARIABLES
+# x = 1
+# y= "2"
+# z= 5
+# username = "John"
+# isCompleted = True
+
+# # print("x")
+# # print(x)
+# # print(username)
+# # print(x+1)
+# # print(x+username)
+
+# # concatenation
+# # print(username+"Bonso")
+# # print(x+int(y))
+
+# # type casting
+# # print(str(x)+z)
+
+# # RULES IN CREATING VARIABLES
+# first name = "Yaw"
+# first_name = "Yaw"
+# firstname = "Yaw"
+
+# bisa1 = ""
+
+# Name = ""
+# name = ""
+# nAme = ""
+
+# garbage collection
+# x = 4
+# x = 3
+# print(x)
+
+# name = "Dert"
+# name = "Yaw"
+# print(name)
+
+
+
+
+
+
+
